8d511fc0e4215184b14a9461e2d26c15b3c9111d
ArrenCelion <rut.nslund@gmail.com>
2025-05-06 12:37:53 +0200

    Add Match Model
    - Update TabletennisER-Quickdbo.txt to accomodate the changes when making the model

---
+++ DataAccessLayer/Models/Match.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class Match
+    {
+        public int MatchId { get; set; }
+        public DateTime MatchDate { get; set; }
+        public int MatchWinner { get; set; }
+        public bool IsSingle { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
