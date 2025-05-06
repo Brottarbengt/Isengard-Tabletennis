a33011396b2f4583e40f16fa70ea1358911d6aab
ArrenCelion <rut.nslund@gmail.com>
2025-05-06 12:39:59 +0200

    Add PlayerMatch Model

---
+++ DataAccessLayer/Models/PlayerMatch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class PlayerMatch
+    {
+        public int PlayerMatchId { get; set; }
+        public int PlayerId { get; set; }
+        public int MatchId { get; set; }
+        public int TeamNumber { get; set; }
+    }
+}
