ab02f8b3a87183cf191d42870d28edb0be9c25a7
Fakhara <fakhara.imran82@gmail.com>
2025-05-12 15:51:11 +0200

    Add Viewmodel

---
+++ Tabletennis/ViewModels/CreateMatchViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tabletennis.ViewModels
+{
+    public class CreateMatchViewModel
+    {
+        [Required]
+        [Display(Name = "Match Date")]
+        public DateTime MatchDate { get; set; } = DateTime.Now;
+
+        [Required]
+        [Display(Name = "Is Singles Match?")]
+        public bool IsSingle { get; set; }
+
+        [Required]
+        [Range(3, 7)]
+        [Display(Name = "Best of (3, 5 or 7 sets)")]
+        public int BestOfSets { get; set; }
+
+        [Required]
+        [Display(Name = "Player 1 Name")]
+        public string Player1Name { get; set; }
+
+        [Required]
+        [Display(Name = "Player 2 Name")]
+        public string Player2Name { get; set; }
+    }
+}
