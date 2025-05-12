f551b440d62bed93027e44b46f22cdc097733a01
Fakhara <fakhara.imran82@gmail.com>
2025-05-12 15:33:39 +0200

    Add DTO

---
+++ DataAccessLayer/DTOs/CreateMatchDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.DTOs
+{
+    public class CreateMatchDTO
+    {
+        public DateTime MatchDate { get; set; }
+        public bool IsSingle { get; set; }
+        public int BestOfSets { get; set; } // e.g., 3, 5, or 7
+
+        public string Player1Name { get; set; }
+        public string Player2Name { get; set; }
+    }
+}
