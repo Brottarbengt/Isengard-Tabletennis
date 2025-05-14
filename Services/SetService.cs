3135d5b79cfdc29e035344901b8da3e914ed3687
Karl Westergren <karlvestergren@gmail.com>
2025-05-14 15:20:14 +0200

    Add SetService to Service Layer

---
+++ Services/SetService.cs
@@ -0,0 +1,25 @@
+﻿using DataAccessLayer.Data;
+using DataAccessLayer.Enums;
+using Microsoft.EntityFrameworkCore;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    
+
+    public class SetService : ISetService
+    {
+        public readonly ApplicationDbContext _dbContext;
+        public SetService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        
+    }
+}
