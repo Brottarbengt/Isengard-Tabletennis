a17ca032d6153e64564523ceef910ec334851579
Karl Westergren <karlvestergren@gmail.com>
2025-05-14 15:20:14 +0200

    Add SetService to Service Layer

--- Tabletennis/Pages/Matches/ActiveMatch.cshtml.cs
+++ Tabletennis/Pages/Matches/ActiveMatch.cshtml.cs
@@ -1,6 +1,8 @@
 using DataAccessLayer.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace Tabletennis.Pages.Matches
 {
@@ -10,9 +12,29 @@ namespace Tabletennis.Pages.Matches
         public DateTime MatchDate { get; set; }
         public int NumberOfSets { get; set; }
         public int SetNumber { get; set; }
+        public Set CurrentSet { get; set; }
+
         public void OnGet()
         {
-            MatchDate = DateTime.Now;
+            if (CurrentSet == null)
+            {
+                StartNewSet();
+            }
+        }
+
+        private void StartNewSet()
+        {
+            // Rensa ModelState för att undvika att gamla värden återkommer
+            ModelState.Clear();
+
+            // Skapa ett nytt set och lägg till det i databasen
+            CurrentSet = new Set();
+            //TODO: Lägga till relevant Service 
+            //_matchService.Sets.Add(CurrentSet);
+            //_matchService.SaveChanges();
+
+            // Återställ Winner till null för nästa omgång
+            //Winner = null;
         }
     }
 }
