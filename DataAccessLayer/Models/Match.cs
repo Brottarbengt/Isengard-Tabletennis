b201192b75131a804ad8599d137265719163fde4
Karl Westergren <karlvestergren@gmail.com>
2025-05-14 11:50:39 +0200

    Add NumberOfSets to Match Model

--- DataAccessLayer/Models/Match.cs
+++ DataAccessLayer/Models/Match.cs
@@ -13,7 +13,7 @@ namespace DataAccessLayer.Models
         public int MatchWinner { get; set; }
         public bool IsSingle { get; set; }
         public bool IsCompleted { get; set; }
-
+        public int NumberOfSets { get; set; }
         public virtual ICollection<PlayerMatch> PlayerMatches { get; set; }
         public virtual ICollection<Set> Sets { get; set; }
     }
