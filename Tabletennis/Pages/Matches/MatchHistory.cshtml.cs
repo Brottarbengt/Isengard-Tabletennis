0064ae45d275308cc01397b27e237e485ee6d980
Fakhara <fakhara.imran82@gmail.com>
2025-05-25 22:53:25 +0200

    Razorpage

--- Tabletennis/Pages/Matches/MatchHistory.cshtml.cs
+++ Tabletennis/Pages/Matches/MatchHistory.cshtml.cs
@@ -1,7 +1,10 @@
 using DataAccessLayer.DTOs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.Infrastructure;
 using Services.Interfaces;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Tabletennis.Pages.Matches
 {
@@ -14,14 +17,32 @@ namespace Tabletennis.Pages.Matches
             _matchService = matchService;
         }
 
-        public List<MatchListDTO> Matches { get; set; } = new();
-
         [BindProperty(SupportsGet = true)]
         public string? Query { get; set; }
 
-        public async Task OnGetAsync()
+        [BindProperty(SupportsGet = true)]
+        public DateTime? SelectedDate { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int PageNumber { get; set; } = 1;
+
+        [BindProperty(SupportsGet = true)]
+        public int PageSize { get; set; } = 3;
+
+        public PagedResult<MatchListDTO> Matches { get; set; } = new();
+
+        public async Task<IActionResult> OnGetAsync()
         {
-            Matches = await _matchService.GetFilteredMatchesAsync(Query);
+            var parameters = new MatchQueryParameters
+            {
+                Query = Query,
+                Date = SelectedDate,
+                PageNumber = PageNumber,
+                PageSize = PageSize
+            };
+
+            Matches = await _matchService.GetFilteredMatchesAsync(parameters);
+            return Page();
         }
     }
 }
