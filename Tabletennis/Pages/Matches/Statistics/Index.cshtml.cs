dad6ff870c6079f4f0a7095e659dd57ec12e32fd
ArrenCelion <rut.nslund@gmail.com>
2025-05-27 11:01:07 +0200

    Add Pages/Matches/Statistics/Index.cshtml RazorPage

---
+++ Tabletennis/Pages/Matches/Statistics/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tabletennis.Pages.Matches.Statistics
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
