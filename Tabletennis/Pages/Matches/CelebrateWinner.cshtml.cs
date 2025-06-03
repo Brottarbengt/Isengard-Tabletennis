5014ae4cd5c74e82141892322e9753864aab29eb
ArrenCelion <rut.nslund@gmail.com>
2025-06-03 10:41:17 +0200

    fix winner video and styling on page

---
+++ Tabletennis/Pages/Matches/CelebrateWinner.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tabletennis.Pages.Matches
+{
+    public class CelebrateWinnerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
